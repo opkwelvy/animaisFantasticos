b43557ad261347795247d10e94a2f1666ca7b146
Kelvy Correa <kelvycorrea35@gmail.com>
2023-01-18 21:55:16 -0300

    Accordion List navagation

---
+++ Accordion list/script.js
@@ -0,0 +1,37 @@
+function initTabNav(){
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabcontent = document.querySelectorAll('.js-tabcontent section');
+
+  if(tabMenu.length && tabcontent.length){
+    tabcontent[0].classList.add('ativo');
+    function activeTab(index){
+      tabcontent.forEach((section)=>{
+      section.classList.remove('ativo')
+      });
+      tabcontent[index].classList.add('ativo');
+    }
+    tabMenu.forEach((itemMenu, index)=>{
+      itemMenu.addEventListener('click', () =>{
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+function initAcorddion(){
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  if(accordionList.length){
+    console.log(accordionList);
+    accordionList[0].classList.add('ativo');
+    accordionList[0].nextElementSibling.classList.add('ativo');
+    function activeAccordion(){
+      this.classList.toggle('ativo');
+      this.nextElementSibling.classList.toggle('ativo');
+    }
+    accordionList.forEach((item) =>{
+      item.addEventListener('click', activeAccordion);
+    });
+  };
+}
+initAcorddion()
