9ec8db7761b8de3a8dd437d1a9dfcc835dd75ea8
Kelvy Correa <kelvycorrea35@gmail.com>
2023-02-09 19:32:46 -0300

    tooltip

--- Parte 2/tooltip/js/script.js
+++ Parte 2/tooltip/js/script.js
@@ -3,7 +3,8 @@ import initAnimacaoScroll from './modules/animacao-scroll.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
-
+import initTooltip from './modules/tooltip.js';
+initTooltip();
 initTabNav();
 initAccordion()
 initAnimacaoScroll();
